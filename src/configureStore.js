bc716f4e490a2af1b0ac15fe14020d6c48fb0522
danielverejan <danie.verejan@gmail.com>
2016-09-26 10:03:57 +0300

    done 17

--- src/configureStore.js
+++ src/configureStore.js
@@ -1,43 +1,44 @@
 import { createStore } from 'redux';
 import todoApp from './reducers';
 
-const addLoggingToDispatch = (store) => {
-  /* eslint-disable no-console */
-  const rawDispatch = store.dispatch;
-  if (!console.group) {
-    return rawDispatch;
-  }
+const logger = (store) => (next) => {
+    if (!console.group) {
+      return next;
+    }
 
-  return (action) => {
-    console.group(action.type);
-    console.log('%c prev state', 'color: gray', store.getState());
-    console.log('%c action', 'color: blue', action);
-    const returnValue = rawDispatch(action);
-    console.log('%c next state', 'color: green', store.getState());
-    console.groupEnd(action.type);
-    return returnValue;
-  };
-  /* eslint-enable no-console */
+    return (action) => {
+      console.group(action.type);
+      console.log('%c prev state', 'color: gray', store.getState());
+      console.log('%c action', 'color: blue', action);
+      const returnValue = next(action);
+      console.log('%c next state', 'color: green', store.getState());
+      console.groupEnd(action.type);
+      return returnValue;
+    };
 };
 
-const addPromisSupportToDispatch = (store) => {
-  const rawDispatch = store.dispatch;
-  return (action) => {
+const promise = (store) => (next) => (action) => {
     if (typeof action.then === 'function') {
-      return action.then(rawDispatch);
+      return action.then(next);
     }
-    return rawDispatch();
-  };
+    return next(action);
+};
+
+const wrapDispatchWithMiddlewares = (store, middlewares) => {
+  middlewares.slice().reverse().forEach(middleware => 
+    store.dispatch = middleware(store)(store.dispatch)
+  );
 };
 
 const configureStore = () => {
   const store = createStore(todoApp);
+  const middlewares = [promise];
 
   if (process.env.NODE_ENV !== 'production') {
-    store.dispatch = addLoggingToDispatch(store);
+    middlewares.push(logger);
   }
 
-  store.dispatch = addPromisSupportToDispatch(store);
+  wrapDispatchWithMiddlewares(store, middlewares);
 
   return store;
 };
